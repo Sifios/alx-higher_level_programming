bbbff1ca3cbb823b78a0e3edc7d22086a059e987
Sifios <114007180+Sifios@users.noreply.github.com>
2023-11-29 04:51:35 +0100

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,16 +7,24 @@
  * @list: pointer to list to check
  * Return: 1 if cyclical, 0 otherwise
  */
-int check_cycle(listint_t *list) {
-    listint_t *slow = list, *fast = list;
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
 
-    while (fast && fast->next) {
-        slow = slow->next;
-        fast = fast->next->next;
-
-        if (slow == fast)
-            return 1;
-    }
-
-    return 0;
+	if (!list)
+	{
+		return (0);
+	}
+	slow = list;
+	fast = list->next;
+	while (fast && slow && fast->next)
+	{
+		if (slow == fast)
+		{
+			return (1);
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
 }
