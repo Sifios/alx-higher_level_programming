9899673ed6ba76675b36d7e11d0561e74a867efe
Sifios <sofianew124@gmail.com>
2023-11-28 18:41:03 -0800

    all basic hello word function

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+First_three_letters = text[:3] 
+Last_two_letters = text[-2:] 
+Middle_word = text[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
