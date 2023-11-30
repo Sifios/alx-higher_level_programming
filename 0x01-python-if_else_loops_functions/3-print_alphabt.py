9240c8d75bd60237b1e934b2f068ededd0f20125
Sifios <114007180+Sifios@users.noreply.github.com>
2023-11-30 05:46:21 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
