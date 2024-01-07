e18d21d5005e8e217371a72ad4171d98096c7138
Sifios <sofianew124@gmail.com>
2024-01-06 18:31:40 -0800

    0x08-python-more_classes

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Solves the N-queens puzzle"""
 
-import sys
+import  sys 
 
 
 def init_board(n):
@@ -94,7 +94,8 @@ def recursive_solve(board, row, queens, solutions):
     Returns:
         solutions
     """
-    if queens == len(board):solutions.append(get_solution(board))
+    if queens == len(board):
+        solutions.append(get_solution(board))
         return (solutions)
 
     for c in range(len(board)):
@@ -102,7 +103,8 @@ def recursive_solve(board, row, queens, solutions):
             tmp_board = board_deepcopy(board)
             tmp_board[row][c] = "Q"
             xout(tmp_board, row, c)
-            solutions = recursive_solve(tmp_board, row + 1,queens + 1, solutions)
+            solutions = recursive_solve(tmp_board, row + 1,
+                                        queens + 1, solutions)
 
     return (solutions)
 
